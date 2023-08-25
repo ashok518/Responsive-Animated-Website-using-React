72746cea2725944767d318b0d3daa6eb9f47928c
ashok518 <48381904+ashok518@users.noreply.github.com>
2023-08-25 10:32:16 +0100

    Add files via upload

---
+++ src/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Comman from './Comman'
+import img from "../src/images/b3.jpg"
+
+const Contact = () => {
+  return (
+    <Comman
+    name="Bootstap Contact Forms"
+    imgname={img}
+    visit="/services"
+   btn="Get Start"
+    />
+  )
+}
+
+export default Contact
---
+++ src/SData.jsx
@@ -0,0 +1,37 @@
+import web1 from "../src/images/a1.jpg";
+import web2 from "../src/images/a2.png";
+import web3 from "../src/images/a2.png";
+import web4 from "../src/images/a12.gif";
+import web5 from "../src/images/b1.jpg";
+import web6 from "../src/images/birds.gif";
+
+
+
+
+const SData=[
+    {
+        imgsrc:web1,
+        title: "web development",
+    },
+    {
+        imgsrc:web2,
+        title: "web development",
+    },
+    {
+        imgsrc:web4,
+        title: "web development",
+    },
+    {
+        imgsrc:web4,
+        title: "web development",
+    },
+    {
+        imgsrc:web5,
+        title: "web development",
+    },
+    {
+        imgsrc:web6,
+        title: "web development",
+    },
+];
+export default SData;
